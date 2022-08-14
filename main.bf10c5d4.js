c2d52ee2c63fd4f7884b3cd2a01d340b2e8ed4b8
tibordp <tibor.djurica@ojdip.net>
2022-08-14 18:26:51 +0000

    Deploying to gh-pages from  @ 36e996dad707910a76cf730577fe9bcf53104165 🚀

---
+++ main.bf10c5d4.js
@@ -0,0 +1,2 @@
+(self.webpackChunkneishauben=self.webpackChunkneishauben||[]).push([[179],{138:(e,n,i)=>{!async function(){"serviceWorker"in navigator&&window.addEventListener("load",(()=>{navigator.serviceWorker.register(i.p+"service-worker.js").then((e=>{console.log("SW registered: ",e)})).catch((e=>{console.log("SW registration failed: ",e)}))}));try{const{initializeNeishauben:e}=await Promise.all([i.e(783),i.e(474)]).then(i.bind(i,474));await e()}finally{document.getElementById("spinner").remove()}}()}},e=>{e(e.s=138)}]);
+//# sourceMappingURL=main.bf10c5d4.js.map
